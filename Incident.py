a0b83871a05b7e708a403bb122c44e88f8b6c9b3
Whefert <jeffersondaley@gmail.com>
2025-04-18 14:53:07 +0100

    Update Incident class

--- Incident.py
+++ Incident.py
@@ -3,7 +3,7 @@ from sqlite3 import Error
 import inquirer
 
 class Incident:
-    def __init__(self,  description, priority, status,incident_id = None, required_resources=None, assigned_resources=None):
+    def __init__(self, description, priority, status,incident_id = None, required_resources=None, assigned_resources=None):
         self.incident_id = incident_id # This will be auto-incremented by the database
         self.priority = priority
         self.required_resources = required_resources if required_resources is not None else []
@@ -29,10 +29,9 @@ class Incident:
             priority=answers['priority'],
             status=answers['status']
         )
-        # Insert the incident into the database
-        incident.create_incident(conn)
-        print(f"Incident created with ID: {incident.incident_id}")
-
+        # Insert the incident into the database and get the incident ID
+        incident.set_incident_id(incident.create_incident(conn))
+        print(f"Incident created with ID: {incident.get_incident_id()}")
 
     def create_incident(self, conn):
         try:
@@ -40,8 +39,36 @@ class Incident:
             cur = conn.cursor()
             cur.execute(sql, (self.incident_id, self.description, self.priority, self.status))
             conn.commit()
+            return cur.lastrowid  # Return the ID of the newly created incident
         except Error as e:
             print(e)    
+
+    # setters and getters for the incident attributes
+    def set_description(self, description):
+        self.description = description
+
+    def get_description(self):
+        return self.description
+    
+    def set_priority(self, priority):
+        self.priority = priority
+    def get_priority(self):
+        return self.priority
+    
+    def set_status(self, status):
+        self.status = status
+    def get_status(self):
+        return self.status
+    
+    def set_incident_id(self, incident_id):
+        self.incident_id = incident_id
+    def get_incident_id(self):
+        return self.incident_id
+    
+    def set_required_resources(self, required_resources):
+        self.required_resources = required_resources
+    def get_required_resources(self):
+        return self.required_resources
 
 
     def __repr__(self):
